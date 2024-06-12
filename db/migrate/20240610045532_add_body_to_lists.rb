51a79c666f17b061e03a780c86a27ff7898a83cd
EC2 Default User <ec2-user@ip-172-31-9-76.ap-northeast-1.compute.internal>
2024-06-12 11:19:53 +0000

    [Finish]3-8

--- db/migrate/20240610045532_add_body_to_lists.rb
+++ db/migrate/20240610045532_add_body_to_lists.rb
@@ -1,5 +1,5 @@
 class AddBodyToLists < ActiveRecord::Migration[7.1]
   def change
-    add_column :lists, :body, :string
+  
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -41,9 +41,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_11_025224) do
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
+    t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "body"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
